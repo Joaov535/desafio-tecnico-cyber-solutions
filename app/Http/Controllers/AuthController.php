fa9ff2ddab450879b583679a5d17026a1f2a9302
Joaov535 <joaovictorcruz535@gmail.com>
2025-06-27 17:58:56 -0300

    realizando login e logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function username()
+    {
+        return 'login';
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'login' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/home');
+        }
+
+        return back()->withErrors([
+            'login' => 'Credenciais inválidas.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login - Cyber Solutions</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
+</head>
+
+<body
+    class="bg-amber-50 dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <div
+        class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <main class="flex max-w-[335px] w-full lg:max-w-4xl lg:flex-row justify-center">
+
+            <form method="POST" action="/login" class="flex flex-col gap-6 border-2 p-10 rounded-2xl bg-white">
+                @csrf
+
+                @if ($errors->any())
+                    <div style="color:red;">
+                        {{ $errors->first() }}
+                    </div>
+                @endif
+
+                <div class="grid gap-6">
+                    <div class="grid gap-2">
+                        <label for="login">Login</label>
+                        <input id="login" name="login" type="text" required autofocus
+                            placeholder="Digite seu login..." value="cyber.solutions" />
+                    </div>
+
+                    <div class="grid gap-2">
+                        <div class="flex items-center justify-between">
+                            <label for="password">Senha</label>
+                        </div>
+                        <input id="password" name="password" type="password" required placeholder="********"
+                            title="Mínimo 8 caracteres" value="teste" />
+                    </div>
+
+                    <button type="submit" class="mt-4 w-full bg-amber-950 text-white">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </main>
+    </div>
+
+    @if (Route::has('login'))
+        <div class="h-14.5 hidden lg:block"></div>
+    @endif
+</body>
+
+</html>
